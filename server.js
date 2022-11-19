10cf08f5ec910355dddd8dc90d95fbaab753321e
Galo98 <galonahuel98@gmail.com>
2022-11-19 14:30:55 -0300

    Iniciando node

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express')
+
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Landing page (PUBLICO)')
+})
+
+// Rutas de registro
+
+app.get('/registro', (req, res) => {
+    res.send('Registro de usuarios (PUBLICO)')
+})
+
+app.post('/registro', (req, res) => {
+    res.send('Va a guardar los datos del usuario registrado')
+})
+
+// Rutas de blog
+
+app.get('/blog', (req, res) => {
+    res.send('Blog de noticias (PUBLICO)')
+})
+
+app.get('/blog/gestion', (req, res) => {
+    res.send('Gestion de blog, donde se puede modificar agregar o borrar noticias (PRIVADO)')
+})
+
+/* ------ Crear noticia ------ */
+app.get('/blog/create', (req, res) => {
+    res.send('Muestra el formulario para crear nuevas noticias (PRIVADO)')
+})
+
+app.post('/blog', (req, res) => {
+    res.send('Guarda las nuevas noticias en la base de datos (PRIVADO)')
+})
+
+/* ------ Editar noticia ------ */
+app.get('/blog/edit', (req, res) => {
+    res.send('Muestra el formulario para editar noticias (PRIVADO)')
+})
+
+app.put('/blog/:id', (req, res) => {
+    res.send('Guarda los cambios de la noticia editada (PRIVADO)')
+})
+
+/* ------ Borrar noticia ------ */
+
+app.delete('/blog/:id', (req, res) => {
+    res.send('Borra una noticia de la base de datos (PRIVADO)')
+})
+
+// Rutas de oportunidades
+
+app.get('/oportunidades', (req, res) => {
+    res.send('Muestra las oportunidades (PUBLICO)')
+})
+
+app.get('/oportunidades/gestion', (req, res) => {
+    res.send('Gestion de oportunidades, donde se puede modificar agregar o borrar oportunidades (PRIVADO)')
+})
+
+/* ------ Crear oportunidad ------ */
+
+app.get('/oportunidades/create', (req, res) => {
+    res.send('Formulario para agregar nuevas oportunidades (PRIVADO)')
+})
+
+app.post('/oportunidades', (req, res) => {
+    res.send('Guarda las nuevas oportunidades en la base de datos (PRIVADO)')
+})
+
+/* ------ Editar oportunidad ------ */
+
+app.get('/oportunidades/edit', (req, res) => {
+    res.send('Muestra el formulario para editar una oportunidad (PRIVADO)')
+})
+
+app.put('/oportunidades/:id', (req, res) => {
+    res.send('Guarda los cambios de la oportunidad editada (PRIVADO)')
+})
+
+/* ------ Borrar oportunidad ------ */
+
+app.delete('/oportunidades/:id', (req, res) => {
+    res.send('Borra una oportunidad de la base de datos(PRIVADO)')
+})
+
+
+const PORT = process.env.PORT
+    app.listen(PORT, (err) => {
+        if (err) throw new Error(`Tuvimos un problema ${err}`)
+        console.log(`Todo ok en el puerto: ${PORT}`)
+    })
